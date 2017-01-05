ab87c9bff09612016f84374fd5d22a07b05ca35c
Mike Qin <mikeandmore@gmail.com>
2017-01-04 22:32:30 -0500

    llvm: add support for gold linker

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -149,7 +149,7 @@ class Llvm < Formula
   unless OS.mac?
     depends_on "gcc" # <atomic> is provided by gcc
     depends_on "glibc" => GlibcRequirement.system_version.to_f >= 2.19 ? :optional : :recommended
-    depends_on "binutils" if build.with? "glibc"
+    depends_on "binutils" # for gold plugin
     depends_on "homebrew/dupes/libedit" # llvm requires <histedit.h>
     depends_on "libxml2"
     needs :cxx11
@@ -260,6 +260,9 @@ class Llvm < Formula
       args << "-DPYTHON_LIBRARY=#{pylib}"
       args << "-DPYTHON_INCLUDE_DIR=#{pyinclude}"
     end
+
+    # Enable llvm gold plugin for LTO
+    args << "-DLLVM_BINUTILS_INCDIR=#{Formula["binutils"].opt_include}" unless OS.mac?
 
     if build.with? "libffi"
       args << "-DLLVM_ENABLE_FFI=ON"
